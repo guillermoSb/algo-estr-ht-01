28dc037a895914a54bf3de06dd017db1b1038f83
maho10 <majix.majito@gmail.com>
2022-01-17 14:19:04 -0600

    File de JUnit creado, inicio creación métodos

--- src/Radio.java
+++ src/Radio.java
@@ -1,2 +1,20 @@
 public interface Radio {
+
+    boolean isOn();
+
+    void turnOnOff();
+
+    void nextStation(boolean station);
+
+    void prevStation(boolean station);
+
+    double getStation();
+
+    void saveStation(int position, double station);
+
+    double getSavedStation(int position);
+
+    boolean getFrequency();
+
+    void switchAMFM();
 }
--- src/RadioSettings.java
+++ src/RadioSettings.java
@@ -1,2 +1,62 @@
-public class RadioSettings {
+
+
+
+public class RadioSettings implements Radio{
+
+    private double amStation;
+    private double fmStation;
+    private boolean turnOn;
+    private final String[] amList = new String[12];
+    private final String[] fmList = new String[12];
+    private boolean frequency;
+
+    @Override
+    public boolean isOn() {
+        return turnOn;
+    }
+
+    @Override
+    public void turnOnOff() {
+        if (turnOn){
+            turnOn = false;
+        } else {
+            turnOn = true;
+        }
+
+    }
+
+    @Override
+    public void nextStation(boolean station) {
+
+    }
+
+    @Override
+    public void prevStation(boolean station) {
+
+    }
+
+    @Override
+    public double getStation() {
+        return 0;
+    }
+
+    @Override
+    public void saveStation(int position, double station) {
+
+    }
+
+    @Override
+    public double getSavedStation(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean getFrequency() {
+        return false;
+    }
+
+    @Override
+    public void switchAMFM() {
+
+    }
 }
