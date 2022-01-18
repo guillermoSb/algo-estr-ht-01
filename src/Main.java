648ee7bbe7537f04828a6482a8eb17a700be0349
Guillermo <guillermo@inlandlogistics.co>
2022-01-18 11:39:14 -0600

    Added Main.java methods.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    /**
+     * Pagina Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        showInfo("Programa de Radio");
+    }
+
+    /**
+     * Mostrar un mensaje de informacion
+     * @param message
+     */
+    public static void showInfo(String message) {
+        System.out.printf("[INFO] %s\n", message);
+    }
 }
